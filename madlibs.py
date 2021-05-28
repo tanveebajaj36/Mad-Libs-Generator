3374f56bf924f3f95a56f14ba90eda36cc70842d
tanveebajaj1234 <81170127+tanveebajaj1234@users.noreply.github.com>
2021-05-28 17:07:13 +0530

    Add files via upload

---
+++ madlibs.py
@@ -0,0 +1,22 @@
+print("Name of the story is: Trip to the Park")
+print(" ")
+person = input("person: ")
+adjective1 = input("adjective1: ")
+adjective2 = input("adjective2: ")
+adjective3 = input("adjective3: ")
+adjective4 = input("adjective4: ")
+noun1 = input("noun1: ")
+noun2 = input("noun2: ")
+verb1 = input("verb1: ")
+verb2 = input("verb2: ")
+verb3 = input("verb3: ")
+
+print(
+    "Yesterday, " + person + " and I went to the park. on our way to the "
+    + adjective1 + " park, we saw a " + adjective2 + noun1 + " on a bike. " +
+    " We also saw big " + adjective3 + " balloons tied to a " + noun2 +
+    ". Once we got to the " + adjective1 + " park, the sky turned " +
+    adjective4 + " It started to " + verb1 + " and " + verb2 + ". " + person +
+    " and I " + verb3 + " all the way home. " + "Tomorrow we will try to go to the "
+    + adjective1 + " park again and hope it dosen't " + verb1
+    )
